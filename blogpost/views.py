20e83559793c7e130fc9ba1ee1cb02aaf987181b
WIrvin77 <wdirvin77@gmail.com>
2020-06-05 13:45:21 -0400

    adding post creation

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from blogpost.forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -12,3 +13,17 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_front_end = {'post': post}
     return render(request, 'blog/post_detail.html', stuff_for_front_end)
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_for_front_end = {'form': form}
+        return render(request, 'blog/post_edit.html', stuff_for_front_end)
