a2d8c97b42823945ce9dc2c01e516ea9fef2b9de
WIrvin77 <wdirvin77@gmail.com>
2020-06-06 17:06:59 -0400

    adding ability to delete comments

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 from blogpost.forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
@@ -77,3 +77,10 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
